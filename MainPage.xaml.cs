4889374a02be02f1973ca25d8c347cffc39eb224
Miki Corral <miki@shopworld.es>
2022-11-29 18:05:55 +0100

    MAUI practice 1 finished

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,62 @@
-﻿namespace ColorMaker;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace ColorMaker;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	bool isRandom;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+		if(!isRandom)
+		{
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+        }
+    }
+
+    private void SetColor(Color color)
+    {
+		btnRandom.BackgroundColor = color;
+		Container.BackgroundColor = color;
+		lblHex.Text = color.ToHex();
+    }
+
+    private void BtnRandom_Clicked(object sender, EventArgs e)
+    {
+		isRandom = true;
+        var random = new Random();
+        
+        var color = Color.FromRgb(
+			random.Next(0, 256), 
+			random.Next(0, 256), 
+			random.Next(0, 256));
+        
+        SetColor(color);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+
+		isRandom = false;
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        await Clipboard.SetTextAsync(lblHex.Text);
+        var toast = Toast.Make("Color copied!", ToastDuration.Short, 12);
+        await toast.Show();
+    }
 }
 
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ColorMaker;
 
@@ -7,13 +8,14 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
+		builder.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .UseMauiCommunityToolkit();
+		
 
 #if DEBUG
 		builder.Logging.AddDebug();
